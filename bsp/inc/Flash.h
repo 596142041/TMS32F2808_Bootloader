1b26f40b19c6ca34beae9d679e05bc10283e6498
596142041 <wcj53636746@126.com>
2018-10-09 21:27:59 +0800

    更改文件说明及其相关信息

--- bsp/inc/Flash.h
+++ bsp/inc/Flash.h
@@ -1,10 +1,8 @@
-/*
- * Flash.h
- *
- *  Created on: 2017年4月16日
- *      Author: admin
- */
-
+/********************************************************
+ *文件名:Flash.h
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
 #ifndef BSP_INC_FLASH_H_
 #define BSP_INC_FLASH_H_
 #include "include.h"
@@ -14,6 +12,5 @@
 #include "Flash280x_API_Library.h"
 void FlashAPI_Init(void);
 Uint16 Flash_RD(Uint32 addr, Uint16 *buffer, Uint16 leng);
-
 Uint16 Flash_WR(Uint32 addr, Uint16 *buffer, Uint16 leng);
 #endif /* BSP_INC_FLASH_H_ */
