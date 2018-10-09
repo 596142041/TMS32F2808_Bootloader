7168b140aea34c17a14394705e05c07c3bd8c80c
596142041 <wcj53636746@126.com>
2018-10-09 21:29:21 +0800

    1.更改文件说明

--- user/inc/main.h
+++ user/inc/main.h
@@ -1,10 +1,8 @@
-/*
- * main.h
- *
- *  Created on: 2017��4��16��
- *      Author: admin
- */
-
+/********************************************************
+ *文件名:main.h
+ *创建时间:2018-10-09
+ *作者: 皇甫仁和
+ ********************************************************/
 #ifndef USER_INC_MAIN_H_
 #define USER_INC_MAIN_H_
 #include "include.h"
@@ -12,6 +10,5 @@
 #include "delay.h"
 #include "stdint.h"
 #include "Flash280x_API_Library.h"
-//--------------------------------
 #include "flash.h"
 #endif /* USER_INC_MAIN_H_ */
